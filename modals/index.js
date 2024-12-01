06547aac8911f09f39517cdcf6bcdeb76f914148
LisaMandarin <chu23001@byui.edu>
2024-12-01 22:44:27 +0800

    connecting to DB works

---
+++ modals/index.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+const connectDB = async (mongodbURI) => {
+    try {
+        await mongoose.connect(mongodbURI)
+        console.log("Connected to MongoDB successfully")
+    } catch (error) {
+        console.error("Failed to connect to MongoDB.  Exiting...", error.message)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
+const cors = require("cors")
+const mongoose = require("mongoose")
+const mongodbURI = process.env.MONGODB_URI
 const port = process.env.PORT
+const connectDB = require("./modals")
+const errorHandling = require("./utils/erorrHandling")
+
+if (!port) {
+    throw new Error("Port is not defined in the env file")
+}
+
+if (!mongodbURI) {
+    throw new Error("Invalid MongoDB URI")
+}
+
+connectDB(mongodbURI)
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
     res.send("Hello World")
 })
+
+app.use(errorHandling)
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
