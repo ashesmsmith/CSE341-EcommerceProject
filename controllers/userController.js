70685a9b32627fd8c665cb2a2af1a26d39d6039d
Ashley Smith <ashesmsmith@gmail.com>
2024-12-04 22:22:14 -0700

    added user rest file/updated users controllers to work with mongoose/updated users address format for ease of use

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,49 +1,200 @@
 const mongoose = require('mongoose');
-const ObjectId = mongoose.ObjectId;
+const ObjectId = mongoose.Types.ObjectId;
 const User = require('../models/userModel');
 
 // Display All Users
 const getUsers = async (req, res, next) => {
     // #swagger.description = 'Display All Users'
-    // #swagger.tags = ['users]
+    
     try {
-        const result = await User.find();
+        const result = await User.find(); 
 
         if (result.length > 0) {
             return res.status(200).json({
                 success: true,
                 data: result
-            })
+            });
         } else {
-            return res.status(200).json({
-                success: true,
-                data: [],
-                message: "No user found"
-            })
+            return res.status(400).json({
+                success: false,
+                message: "No users found."
+            });
         }
     } catch (error) {
-        next(error)
+        next(error);
+
     }
 };
 
-// Display Single User
+// Display Single User By Id
 const getUserById = async (req, res, next) => {
+    // #swagger.description = 'Display Single User By Id'
 
+    try {
+        const userId = req.params.id;
+
+        if (!ObjectId.isValid(userId)) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid UserId."
+            });
+        } else {
+            const result = await User.findById(userId);
+
+            if (!result) {
+                return res.status(400).json({
+                    success: false,
+                    message: "User not found."
+                })
+            } else {
+                return res.status(200).json({
+                    success: true,
+                    data: result
+                });
+            }
+        }
+
+    } catch (error) {
+        next(error);
+    }
 };
 
 // Create New User
 const createUser = async (req, res, next) => {
+    // #swagger.description = 'Create a new user'
 
+    try {
+        const { 
+            firstName, 
+            lastName, 
+            email, 
+            phone,
+            street, 
+            city, 
+            state, 
+            zipCode, 
+            accountType} = req.body;
+
+        if (!firstName || !lastName || !email || !phone || !street || !city || !state || !zipCode || !accountType) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required."
+            });
+        }
+
+        const newUser = new User({ 
+            firstName, 
+            lastName, 
+            email,
+            phone,
+            street, 
+            city, 
+            state, 
+            zipCode, 
+            accountType })
+
+        const result = await newUser.save()
+        return res.status(201).json ({
+            success: true,
+            data: result
+        });
+    } catch (error) {
+        next(error);
+    }
 };
 
 // Update User
 const updateUser = async (req, res, next) => {
+    // #swagger.description = 'Update User'
 
-};
+    try {
+        if (!ObjectId.isValid(req.params.id)) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid UserId."
+            })
+        }
+        const userId = req.params.id
+
+        const {
+            firstName, 
+            lastName, 
+            email,
+            phone,
+            street, 
+            city, 
+            state, 
+            zipCode, 
+            accountType} = req.body
+
+        if (!firstName || !lastName || !email || !phone || !street || !city || !state || !zipCode || !accountType) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required."
+            })
+        }
+
+        const updatedUser = new User({ 
+            firstName, 
+            lastName, 
+            email, 
+            phone,
+            street, 
+            city, 
+            state, 
+            zipCode, 
+            accountType })
+
+        const result = await User.findById(userId, updatedUser, {new: true})
+        if (!result) {
+            return res.status(404).json({
+                success: false,
+                message: `User ${userId} not found`
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            data: result,
+            message: `User ${userId} successfully updated`
+        })
+
+    } catch (error) {
+        next(error);
+    }
+}
 
 // Delete User
 const deleteUser = async (req, res, next) => {
+    // #swagger.description = 'Delete User'
 
+    try {
+        if (!ObjectId.isValid(req.params.id)) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid UserId"
+            });
+        }
+    
+        const userId = new ObjectId(req.params.id)
+        const result = await User.findOneAndDelete({ _id: userId })
+
+        if (result.deletedCount === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: `User ${userId} deleted successfully`,
+            result
+        });
+        
+    
+    } catch (error) {
+        next(error);
+    }
 };
 
 module.exports = { getUsers, getUserById, createUser, updateUser, deleteUser }
--- models/userModel.js
+++ models/userModel.js
@@ -6,44 +6,34 @@ const userSchema = new Schema(
             type: String,
             required: true
         },
-
         lastName: {
             type: String,
             required: true
         },
-
         email: {
             type: String,
             required: true
         },
-
         phone: {
             type: Number,
             required: true
         },
-
-        address: [{
-            street: {
-                type: String,
-                required: true
-            },
-
-            city: {
-                type: String,
-                required: true
-            },
-
-            state: {
-                type: String,
-                required: true
-            },
-
-            zipCode: {
-                type: Number,
-                required: true
-            },
-        }],
-
+        street: {
+            type: String,
+            required: true
+        },
+        city: {
+            type: String,
+            required: true
+        },
+        state: {
+            type: String,
+            required: true
+        },
+        zipCode: {
+            type: Number,
+            required: true
+        },
         accountType: {
             type: String,
             required: true,
