13da2cd0ecf6c3b3073ed3a0951dbc53907ddbcb
Ashley Smith <ashesmsmith@gmail.com>
2024-12-07 21:38:03 -0700

    user updates/attempt to fix updateuser function

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,13 +4,21 @@ const { checkAuth, checkAdmin } = require('../utils/Oauth');
 const { userRules, validateUser } = require('../utils/userValidation');
 
 // Display All Users (Admin Only)
-router.get('/', checkAuth, checkAdmin, userController.getUsers);
+router.get('/', 
+  checkAuth, 
+  checkAdmin, 
+  userController.getUsers);
 
 // Display User by Id (Admin or User Only)
-router.get('/:id', checkAuth, userController.getUserById);
+router.get('/:id', 
+  checkAuth, 
+  userController.getUserById);
 
 // Create New User (All Access)
-router.post('/', userRules, validateUser, userController.createUser);
+router.post('/', 
+  userRules, 
+  validateUser, 
+  userController.createUser);
 
 // Update User (Admin or User Only)
 router.put(
@@ -22,6 +30,8 @@ router.put(
 );
 
 // Delete User (Admin or User Only)
-router.delete('/:id', checkAuth, userController.deleteUser);
+router.delete('/:id', 
+  checkAuth, 
+  userController.deleteUser);
 
 module.exports = router;
